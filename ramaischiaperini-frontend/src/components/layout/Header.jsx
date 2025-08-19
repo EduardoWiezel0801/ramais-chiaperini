f7ba05afc3c5b4c3cc5914f36f724d058f273fab
Tesserato Integra <tesseratointegra@gmail.com>
2025-08-19 10:09:19 -0300

    v3

--- ramaischiaperini-frontend/src/components/layout/Header.jsx
+++ ramaischiaperini-frontend/src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ function Header({ user, onLogout }) {
   return (
     <header className="header">
       <div className="header-content">
-        <h1 className="logo">Sistema de Ramais - Chiaperini</h1>
+        <h1 className="logo">Ramais</h1>
         <div className="user-info">
           <span className="user-name">Olá, {user.username}</span>
           <button className="logout-btn" onClick={onLogout}>
